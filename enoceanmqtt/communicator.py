f2480e2ebc27c1697dadc1dd9fc699556bafbda1
twollweber <thorsten.wollweber@airbus.com>
2025-02-27 22:43:46 +0100

    Update communicator.py

--- enoceanmqtt/communicator.py
+++ enoceanmqtt/communicator.py
@@ -362,7 +362,7 @@ class Communicator:
                     # Handling received data packet
                     found_property = self._handle_data_packet( packet, cur_sensor, mqtt_json)
                     if not found_property:
-                        logging.warning("message not interpretable: %s", cur_sensor['name'])
+                        logging.warning("Mmessage not interpretable: %s", cur_sensor['name'])
                         logging.warning("Packet: %s", packet)
                     else:
                         self._publish_mqtt(cur_sensor, mqtt_json)
